1762c635b4bfbdfa8ff97e9407e40c436410cb28
Kweku Adams <javaman9@gmail.com>
2024-08-29 11:35:08 -0500

    Fix copyright headers and package since these are new files.

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2024
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cmd
+package fetch
 
 import (
 	"bytes"
